85f4078a7a86d82bc4896b14ca707ad829f360ce
Alexander <elektrik.current@gmail.com>
2020-11-29 00:29:25 +0100

    [UI] Added filter by substring on vocabulary page

--- project/public/js/vocabulary.js
+++ project/public/js/vocabulary.js
@@ -3,6 +3,18 @@ $(document).ready(function() {
     return window.confirm('Do you really want to delete this words?');
        
   });
+  $("#word-filter").on("keyup", function() {
+    var value = this.value.toLowerCase().trim();
+    $("#word-list li").show().filter(function() {
+      return $(this).text().toLowerCase().trim().indexOf(value) == -1;
+    }).hide();
+  });
+  
+  $("#word-filter-reset").click(function() {
+    $('#word-filter').val('');
+    $("#word-list li").show();
+    
+  });
 
 });
 
