28670624e41dbb96f7df4953d42c8bd509b0f42d
Alexander <elektrik.current@gmail.com>
2020-12-01 01:21:29 +0100

    [UI,Backend] Added training stats to the vocabulary page

--- project/routes/words.js
+++ project/routes/words.js
@@ -7,14 +7,75 @@ var TrainLog = mongoose.model('TrainLog');
 
 router.get('/words', function(req, res) {
 
+  var train_stats = [];
+  TrainLog.aggregate([
+    {
+      $group: {
+        "_id": {
+          "word_id": "$word_id"
+        },
+        train_result_yes: {
+          $sum: {
+            $cond: {
+              if: {
+                $eq: [
+                  "$train_result",
+                  true
+                ]
+              },
+              then: 1,
+              else: 0
+            }
+          }
+        },
+        train_result_no: {
+          $sum: {
+            $cond: {
+              if: {
+                $eq: [
+                  "$train_result",
+                  false
+                ]
+              },
+              then: 1,
+              else: 0
+            }
+          }
+        }
+      }
+    },
+    {
+      $sort: {"train_result_yes": -1 }
+    },
+  ]).exec( function(err, res) {
+    if(err){
+      res.render('error', {error:err});
+    }
+  
+    if(res.length > 0) {
+      for(i=0; i<res.length; i++) {
+        train_stats[ res[i]._id.word_id ] = res[i];
+        
+        train_stats[ res[i]._id.word_id ].success_rate = 0;
+        if(res[i].train_result_yes + res[i].train_result_no > 0){
+          train_stats[ res[i]._id.word_id ].success_rate = Math.round(100*res[i].train_result_yes/(res[i].train_result_yes + res[i].train_result_no));
+        }
+        
+      }
+    }
+  
+    console.log(train_stats);
+  });
+
+
   Vocabulary.find().sort({original: 1}).exec(function(err, words){
     if(err) {
       res.render('error', {error:err});
     }
-    console.log(words)
+    //console.log(words)
     res.render(
       'words',
-      {title : 'WordRepeater - Vocabulary', words : words}
+      {title : 'WordRepeater - Vocabulary', words : words, train_stats: train_stats}
     );
   });
   //res.send('Just a test');
