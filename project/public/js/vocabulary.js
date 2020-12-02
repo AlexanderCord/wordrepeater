6d60f905f36e65063af4e16578ab28c141022bc4
Alexander <elektrik.current@gmail.com>
2020-12-02 02:02:06 +0100

    [UI] Words - filter by first character

--- project/public/js/vocabulary.js
+++ project/public/js/vocabulary.js
@@ -15,6 +15,55 @@ $(document).ready(function() {
     $("#word-list li").show();
     
   });
+  
+  $(function() {
+    for (i = 0; i < 26; i++) {
+    
+      letter = (i+10).toString(36).toUpperCase();
+      console.log('F' + letter);
+      var link = $('<a></a>');
+      link.attr('href', '#letter=' + letter);
+      link.text(letter);
+      $('#letter-filter').append(link);
+      if(i <= 24) $('#letter-filter').append(' | ');
+    }
+  });
+  
+
+  function filterByFirstCharacter(char) {
+    $("#word-list li").show().filter(function() {
+      return $(this).text().trim().substring(0,1).toLowerCase() !== char.toLowerCase();
+    }).hide()          
+  }
+
+  function hashChanged(hash) {
+    var fn = hash.replace('#', "").split('&');
+  
+    if(fn.length) {
+      for (var i = 0; i < fn.length; i++) {
+        pair = fn[i].split("=");
+        param = pair[0];
+        val = pair[1];
+        
+        if(param == "letter" && val.length == 1) {
+          filterByFirstCharacter(val);
+
+        }
+        
+        console.log(param  + '=' + val);
+        
+        
+      }
+    }
+  }
+  var storedHash = window.location.hash;
+  window.setInterval(function () {
+    if (window.location.hash != storedHash) {
+      storedHash = window.location.hash;
+      hashChanged(storedHash);
+    }
+  }, 100);
 
 });
+
 
