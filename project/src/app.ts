07f7629b2e8aab8a617e0ebea72b4d4d4d2dcf24
Alexander <elektrik.current@gmail.com>
2020-12-15 22:17:43 +0100

    Added TS config and app draft

---
+++ project/src/app.ts
@@ -0,0 +1,31 @@
+import express, { Express } from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import bodyParser from 'body-parser';
+// import todoRoutes from './routes'
+
+const app: Express = express()
+
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(bodyParser.json())
+app.use(cors())
+//app.use(todoRoutes)
+
+const uri: string = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@localhost/${process.env.MONGO_DB}?retryWrites=true&authSource=admin`
+console.log(uri);
+const options = { useNewUrlParser: true, /* useUnifiedTopology: true */}
+mongoose.set('useFindAndModify', false)
+/*
+mongoose
+    .connect(uri, options)
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        )
+    )
+    .catch((error) => {
+        throw error
+    })
+*/
